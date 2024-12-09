b395964bb567f7fbb0a159d4937310ddc652e218
Artem <agaleev583@gmail.com>
2024-12-09 16:48:57 +0300

    feat:hz

--- script.js
+++ script.js
@@ -34,3 +34,65 @@ const ball = {
     dy: -ballSpeed,
     isResetted: false,
 }
+
+function renderBall() {
+    canvas.fillRect(ball.x, ball.y, ball.width, ball.height);
+}
+
+function moveBall() {
+    ball.x += ball.dx;
+    ball.y += ball.dy;
+}
+
+function renderLeftPaddle() {
+    canvas.fillRect(leftPaddle.x, leftPaddle.y, leftPaddle.width, leftPaddle.height);
+}
+
+function renderRightPaddle() {
+    canvas.fillRect(rightPaddle.x, rightPaddle.y, rightPaddle.width, rightPaddle.height);
+}
+
+function movePaddles() {
+    leftPaddle.y += leftPaddle.dy;
+    rightPaddle.y += rightPaddle.dy;
+}
+
+function renderMap() {
+    canvas.fillRect(0, 0, map.width, grid); // Верхняя граница
+    canvas.fillRect(0, map.height - grid, map.width, grid) // Нижняя граница
+
+    for (let i = grid; i < map.height - grid; i += grid * 2) {
+        canvas.fillRect(map.width / 2, i, grid, grid); // Разделительная линия
+    }
+}
+
+function clearMap() {
+    canvas.clearRect(0, 0, map.width, map.height);
+}
+
+function loop() {
+    clearMap();
+    renderLeftPaddle();
+    renderRightPaddle();
+    movePaddles();
+    renderBall();
+    moveBall();
+    renderMap();
+    requestAnimationFrame(loop);
+}
+
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'w') {
+        leftPaddle.dy = -paddleSpeed;
+    }
+    else if (event.key === 's') {
+        leftPaddle.dy = paddleSpeed;
+    }
+});
+document.addEventListener('keyup', (event) => {
+    if (event.key === 'w' || event.key === 's') {
+        leftPaddle.dy = 0;
+    }
+})
+
+requestAnimationFrame(loop);
